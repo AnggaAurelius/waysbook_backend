4387ddfcfbc3850472ffa91756d8e423bdd5b3dc
Angga Afrianto <anggaaurelius@gmail.com>
2021-03-02 18:58:13 +0700

    seeder

---
+++ seeders/20210302104616-admin.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("users", [
+      {
+        fullName: "aku",
+        email: "anggaaurelius@gmail.com",
+        password: "12345678",
+        role: "ADMIN",
+        gender: "Male",
+        phone: "1234567",
+        address: "mojokerto",
+        avatar: "profile.png",
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {},
+};
