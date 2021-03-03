f552e7d32b5dba3f17d8cc3452beb6d69a23986d
Angga Afrianto <anggaaurelius@gmail.com>
2021-03-03 10:16:16 +0700

    Update 20210302104616-admin.js

--- seeders/20210302104616-admin.js
+++ seeders/20210302104616-admin.js
@@ -19,6 +19,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("users", null, {});
+    return queryInterface.bulkDelete("Users", null, {});
   },
 };
