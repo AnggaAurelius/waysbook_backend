bbc1ba214b9bd10c5b4775181dd56417a3b215da
Angga Afrianto <anggaaurelius@gmail.com>
2021-03-05 20:02:56 +0700

    Update 20210302104616-admin.js

--- seeders/20210302104616-admin.js
+++ seeders/20210302104616-admin.js
@@ -1,7 +1,24 @@
 "use strict";
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {},
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", [
+      {
+        fullName: "aku",
+        email: "anggaaurelius@gmail.com",
+        password:
+          "$2b$10$k4oZR/7WRr2hG0ue3hKBiOj7YIjHAmQV1OvO4q9xA4.XFI1G3X/pe",
+        role: "ADMIN",
+        gender: "Male",
+        phone: "1234567",
+        address: "mojokerto",
+        avatar:
+          "https://res.cloudinary.com/anggafile/image/upload/v1614783546/images/profile_bo2dta.png",
+        createdAt: "2017-01-01",
+        updatedAt: "2019-01-11",
+      },
+    ]);
+  },
 
   down: async (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("Users", null, {});
