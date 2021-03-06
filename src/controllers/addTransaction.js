bcb5e139cdb79abc614087e64f119b864813b5ae
Angga Afrianto <anggaaurelius@gmail.com>
2021-03-06 11:26:35 +0700

    Update addTransaction.js

--- src/controllers/addTransaction.js
+++ src/controllers/addTransaction.js
@@ -8,11 +8,11 @@ const {
 
 exports.addTransaction = async (req, res) => {
   try {
-    //   const cart = await Cart.findAll({
-    //   where: {
-    //     user: req.user.id,
-    //   },
-    // });
+    const cart = await Cart.findAll({
+      where: {
+        user: req.user.id,
+      },
+    });
     const sum = await sumItem.findOne({
       where: {
         user: req.user.id,
@@ -27,39 +27,39 @@ exports.addTransaction = async (req, res) => {
       sum: sum.pay,
       payment: "Pending",
     });
-    // const transaction = await Transaction.findOne({
-    //   where: {
-    //     attachment: newTransaction.attachment,
-    //   },
-    //   include: {
-    //     as: "user",
-    //     model: User,
-    //     attributes: {
-    //       exclude: [
-    //         "password",
-    //         "createdAt",
-    //         "updatedAt",
-    //         "phone",
-    //         "address",
-    //         "role",
-    //       ],
-    //     },
-    //   },
-    //   attributes: {
-    //     exclude: ["userId", "createdAt", "updatedAt"],
-    //   },
-    // });
-    // var x = 0;
-    // const length = cart.length;
-    // while (x < length) {
-    //   await PurchasedBook.create({
-    //     bookId: cart[x].bookId,
-    //     transaction: transaction.id,
-    //     user: req.user.id,
-    //     status: "false",
-    //   });
-    //   x++;
-    // }
+    const transaction = await Transaction.findOne({
+      where: {
+        id: newTransaction.id,
+      },
+      include: {
+        as: "user",
+        model: User,
+        attributes: {
+          exclude: [
+            "password",
+            "createdAt",
+            "updatedAt",
+            "phone",
+            "address",
+            "role",
+          ],
+        },
+      },
+      attributes: {
+        exclude: ["userId", "createdAt", "updatedAt"],
+      },
+    });
+    var x = 0;
+    const length = cart.length;
+    while (x < length) {
+      await PurchasedBook.create({
+        bookId: cart[x].bookId,
+        transaction: transaction.id,
+        user: req.user.id,
+        status: "false",
+      });
+      x++;
+    }
 
     res.send({
       data: {
